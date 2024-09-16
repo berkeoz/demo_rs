f8235e72d8412f97fb96b4f204fd987fd5680172
berkeoz <berkeoz>
2024-09-16 16:49:42 +0000

    Commit1

---
+++ dbo/Functions/Creation-Scripts/GetFullName.sql
@@ -0,0 +1,14 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION GetFullName (@EmployeeID INT)
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @FullName NVARCHAR(100);
+    SELECT @FullName = FirstName + ' ' + LastName FROM Employees WHERE EmployeeID = @EmployeeID;
+    RETURN @FullName;
+END;
+GO
